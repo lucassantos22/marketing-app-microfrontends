7f11e9111e30efdf1f34a48d4c18464a98e683e2
lucassantos22 <lucas.afsantos@hotmail.com>
2023-07-26 21:56:44 -0300

    feat: add botstrat marketing file

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>Hi there!</h1>,
+        el
+    )
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#marketing-dev')
+
+    if (el) {
+        mount(el)
+    }
+}
+
+export { mount }
